bb10fd6633a234a4b1894870ed5ff5a7b65e1130
alexander-entin <alexander.entin@gmail.com>
2022-10-21 22:52:46 +0300

    +senators -minor parties

--- index.js
+++ index.js
@@ -17,17 +17,21 @@ app.all('/candidates', async (req, res) => {
         let root = `${base}/my-ballot?${req._parsedUrl.query}`
         let ballot = await got(root).text()
         let $ = cheerio.load(ballot)
-        let h3 = $('h3:contains("U.S. Rep., Dist.")')
+        let h3 = $('h3:contains("U.S. Senator"), h3:contains("U.S. Rep., Dist.")')
         let div = h3.parent().parent().next()
-        let as = div.find('h4 a[href^="/candidate?"]')
+        let party = div.find('span.party:contains("Democrat"), span.party:contains("Republican")')
+        let as = party.parent().prev().find('a[href^="/candidate?"]')
         let urls = as.map((i, x) => x.attribs.href).get()
         let pages = await Promise.all(urls.map(u => got(base + u).text()))
         let candidates = pages.map(page => {
             let $ = cheerio.load(page)
-            let name = $('h2.hero-title div').text()
+            let name = $('dt:contains("Name")').next().text()
+            let party = $('dt:contains("Party")').next().text()
+            let race = $('dt:contains("Race")').next().text().slice(5).split('.')[0]
+            let incumbent = $('dt:contains("Incumbent")').next().text()
             let photo = $('header.profile-hero img').attr('src')
             let links = $('dd > a, div.dropdown-inner > a').map((i, x) => x.attribs.href).get()
-            return { name, photo, links }
+            return { name, party, race, incumbent, photo, links }
         })
         console.log(req.method, req.url, candidates.length, Math.round(Date.now() - start))
         res.send(candidates)
