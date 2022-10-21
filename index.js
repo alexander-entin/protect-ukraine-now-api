f721b90106712db25d2f1f7f61a0ed8131b55fa3
alexander-entin <alexander.entin@gmail.com>
2022-10-21 19:42:27 +0300

    for older nodejs

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.all('/', (req, res) => {
 
 
 app.all('/candidates', async (req, res) => {
-    // console.log(req.method, req.url)
-    let start = performance.now()
     try {
+        // console.log(req.method, req.url)
+        let start = Date.now()
         let base = 'https://ivoterguide.com'
         let root = `${base}/my-ballot?${req._parsedUrl.query}`
         let ballot = await got(root).text()
@@ -26,10 +26,10 @@ app.all('/candidates', async (req, res) => {
             let $ = cheerio.load(page)
             let name = $('h2.hero-title div').text()
             let photo = $('header.profile-hero img').attr('src')
-            let links = $('dd > a').map((i, x) => x.attribs.href).get()
+            let links = $('dd > a, div.dropdown-inner > a').map((i, x) => x.attribs.href).get()
             return { name, photo, links }
         })
-        console.log(req.method, req.url, candidates.length, Math.round(performance.now() - start))
+        console.log(req.method, req.url, candidates.length, Math.round(Date.now() - start))
         res.send(candidates)
     } catch (error) {
         console.error(req.method, req.url, error)
